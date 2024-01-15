5209d071293d15b08effe1a81dae94ca2dce395e
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-15 12:54:30 +0100

    Fix: fixed input

--- src/components/Buttons/ForgotPasswordButton.tsx
+++ src/components/Buttons/ForgotPasswordButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { StyledButtonProps } from './types';
+import { StyledButtonProps } from '../types';
 
 
 export const ForgotPasswordButton = (props: StyledButtonProps) => {
