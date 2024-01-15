380398b3927b580845106762a2d7cf4f1aaa9ab2
victoriakallerud <victoriakallerud@gmail.com>
2024-01-15 16:01:43 +0100

    feat (#4): moved fetching of payments into new component SplitScreen

--- App.tsx
+++ App.tsx
@@ -7,10 +7,8 @@ import { SmallText } from "./src/components/Text/SmallText";
 import { MediumText } from './src/components/Text/MediumText';
 import { LargeText } from './src/components/Text/LargeText';
 import { XSmallText } from './src/components/Text/XSmallText';
-import { db } from './firebaseConfig';
-import { DocumentSnapshot, collection, getDocs } from 'firebase/firestore'
-import Payment from './src/components/Payment';
-import { PaymentType } from './src/utils/types';
+
+import SplitScreen from './src/screens/SplitScreen';
 
 
 
@@ -18,25 +16,6 @@ import { PaymentType } from './src/utils/types';
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
-  const [data, setData] = useState<PaymentType[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const payments = collection(db, 'Payments');
-        const response = await getDocs(payments);
-        console.log(response)
-        const items = response.docs.map((doc: DocumentSnapshot) => ({...doc.data(), id: doc.id} as PaymentType));
-        setData(items)
-        console.log(items)
-
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
-
-    fetchData();
-  }, []); 
 
   useEffect(() => {
     async function loadFonts() {
@@ -58,30 +37,13 @@ export default function App() {
     <View style={styles.container}>
       <Image source={require('./src/utils/cards.webp')} style={styles.image}/>
 
-      {/* <LargeText children={"Money Split"}/>
+      <LargeText children={"Money Split"}/>
       <MediumText children={"Money Split"}/>
       <SmallText children={"Money Split"}/>
-      <XSmallText children={"Nå skriver jeg inn en hel masse her bare for å se hvordan det vil bli seende ut på skjermen. Kanskje blir det stygt. Kanskje blir det fint."}/> */}
+      <XSmallText children={"Nå skriver jeg inn en hel masse her bare for å se hvordan det vil bli seende ut på skjermen. Kanskje blir det stygt. Kanskje blir det fint."}/>
       
       <StatusBar style="auto" />
 
-      {Object.entries(data).map((e) => {
-        return(
-        <Payment 
-          id={Object(e[0])}
-          partOfPayment={false} 
-          yourPartIs={Object.keys(e[1].participants)[0]} 
-          payment={{
-            id: Object(e[0]),
-            creator: Object(e[1].creator),
-            sumOfPayment: Object(e[1].sumOfPayment),
-            currency: Object(e[1].currency),
-            title: Object(e[1].title),
-            participants: Object(e[1].participants),
-            relatedSplitsId: Object(e[1].relatedSplitsId)
-          }} 
-        />)
-        })}
     </View>
   );
 }
