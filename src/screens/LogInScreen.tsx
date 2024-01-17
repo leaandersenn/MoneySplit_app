7afa22da1c83d878d693bc16b0aca9a2ab38a95d
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-17 18:39:18 +0100

    Feat: github login

--- src/screens/LogInScreen.tsx
+++ src/screens/LogInScreen.tsx
@@ -1,104 +1,102 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { LargeText } from '../components/Text/LargeText';
-import {signInWithEmailAndPassword } from "firebase/auth";
-import { FIREBASE_AUTH } from '../../firebaseConfig';
-import { MediumText } from '../components/Text/MediumText';
-import { GreenLargeButton } from '../components/Buttons/GreenLargeButton';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { XSmallText } from '../components/Text/XSmallText';
-import { SignInButton } from '../components/Buttons/SignInButton';
+import { Text, View } from "react-native";
+import { GithubAuthProvider, signInWithCredential } from "firebase/auth";
+import * as React from "react";
+import { FontAwesome as Icon } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import { createTokenWithCode } from "../utils/createGitHubToken";
+import { Image, StyleSheet, useColorScheme } from "react-native";
 
-import { ForgotPasswordButton } from '../components/Buttons/ForgotPasswordButton';
-import DividerWithText from '../components/Divider';
-import Spacer from '../components/Spacer';
-import TextInputField from '../components/InputFields/TextInputField';
-import PasswordInput from '../components/InputFields/PasswordInput';
 
-type RootStackParamList = {
-    LogIn: undefined;
-    SignUp: undefined;
-  };
-  
-  type LogInScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'LogIn'>;
-  
-  type Props = {
-    navigation: LogInScreenNavigationProp;
-  };
+WebBrowser.maybeCompleteAuthSession();
 
-export default function LogInScreen({ navigation}: Props) {
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const auth = FIREBASE_AUTH;
-
-    const signIn = async () => {
-        try {
-            const response = await signInWithEmailAndPassword(auth, email, password);
-            console.log(response);
-        } catch (error) {
-            console.log(error);
-            alert('Email or password is wrong');
-    }
-}
-
-    const forgotPassword = () => {
-        console.log("Forgot password")
-        /* Handle forgot password */
-    }
-  
-    return (
-        <View style={styles.container}>
-            <View style={styles.topText}>
-                <LargeText children={"MoneySplit"} />
-                <MediumText children={"Sign In"} />
-            </View>
-           
-            <TextInputField value={email} placeholder='Email' onChangeText={setEmail} />
-            <PasswordInput  value={password} placeholder='Password' onChangeText={setPassword} />
-            <Spacer size={24} horizontal={false}/>
-           
-            <View style={styles.rememberForgot}>
-                <XSmallText children={"Remember me"}/>
-                <Spacer size={14} horizontal={true} />
-                <ForgotPasswordButton title={"Forgot Password?"} onClick={forgotPassword} />  
-            </View>
-            
-            <GreenLargeButton title='Sign In' onClick={signIn} />
-            <DividerWithText title={"Or login with"}/>
-
-            {/* TODO: ADD in Google Component*/}
-            <GreenLargeButton title='Log In with Google' onClick={signIn} />
-
-            <View style={styles.registeredText}>
-                <XSmallText children={"Don´t have an account? "} />
-                <SignInButton title={"Sign Up"} onClick={() => navigation.navigate('SignUp')} />
-            </View>
-        </View>
-    )
+// Endpoint
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint: `https://github.com/settings/connections/applications/${process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID}`,
 };
 
-const styles = StyleSheet.create({
-    container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default function Index(): JSX.Element {
+  const currentTheme = useColorScheme();
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID!,
+      scopes: ["identity", "user:email", "user:follow"],
+      redirectUri: makeRedirectUri(),
     },
-    rememberForgot: {
-        flexDirection: 'row',
-        alignContent: 'center',
-    },
-    registeredText: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginTop: 35,
-        color: '#808080'
-    },
-    topText: {
-        marginBottom: 56,
-        gap: 36,
-        alignItems: 'center'
+    discovery
+  );
+
+  React.useEffect(() => {
+    handleResponse();
+  }, [response]);
+
+  async function handleResponse() {
+    if (response?.type === "success") {
+      const { code } = response.params;
+      const { token_type, scope, access_token } = await createTokenWithCode(
+        code
+      );
+      console.log("getGithubTokenAsync: ", {
+        token_type,
+        scope,
+        access_token,
+      });
+
+      if (!access_token) return;
+      const credential = GithubAuthProvider.credential(access_token);
+      const data = await signInWithCredential(FIREBASE_AUTH, credential);
+
+      fetch("https://api.github.com/user/following/betomoedano", {
+        method: "PUT",
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+        .then((response) => {
+          if (response.status === 204) {
+            console.log("Successfully followed!");
+          } else {
+            console.log("Failed to follow.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error following user:", error);
+        });
+
+      console.log("credential: ", credential);
+      console.log("data: ", JSON.stringify(data, null, 2));
     }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ marginBottom: 90 }}>Visit codewithbeto.dev today!</Text>
+      <View>
+        <Icon.Button
+          name="github"
+          color={currentTheme === "dark" ? "white" : "black"}
+          backgroundColor="transparent"
+          size={30}
+          onPress={() => {
+            promptAsync({ windowName: "Code with Beto" });
+          }}
+        >
+          <Text style={{ fontSize: 17, fontWeight: "500" }}>
+            Sign In with Github
+          </Text>
+        </Icon.Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
