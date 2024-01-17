c3f3bd60727a542857125ccb0d1cbe3ee95cb30d
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-17 22:18:55 +0100

    Feat: Test screen to check login

--- src/screens/AfterLogInScreen.tsx
+++ src/screens/AfterLogInScreen.tsx
@@ -1,9 +1,25 @@
 import { Text, View, StyleSheet, Button } from "react-native";
 import { FIREBASE_AUTH } from "../../firebaseConfig";
 import SignOutButton from "../components/Buttons/SignOutButton";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+type RootStackParamList = {
+    Home: undefined;
+    LogIn: undefined;
+    SignUp: undefined;
+    AfterLogin: undefined;
+    HomeScreen: undefined;
+  };
+  
+  type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+  
+  type Props = {
+    navigation: HomeScreenNavigationProp;
+  };
+  
 
 
-export default function AfterLoginScreen() {
+export default function AfterLoginScreen( {navigation}: Props) {
     const user = FIREBASE_AUTH.currentUser
     if (user) {
     console.log('User email: ', user.email);
@@ -11,7 +27,7 @@ export default function AfterLoginScreen() {
     return(
         <View style={styles.container}>
             <Text>Du er logget inn som: {user?.email}</Text>
-            <SignOutButton />
+            <SignOutButton navigation={navigation} />
         </View>
     )
 }
