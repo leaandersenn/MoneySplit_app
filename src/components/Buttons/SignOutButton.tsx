e28c7bbda99bd3be6c682ee41a3a8ff13edc483d
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-17 23:35:53 +0100

    Fix: signout navigation fix.

--- src/components/Buttons/SignOutButton.tsx
+++ src/components/Buttons/SignOutButton.tsx
@@ -1,39 +1,16 @@
 import { StyleSheet, TouchableOpacity, Text } from "react-native";
 import { getAuth, signOut } from 'firebase/auth';
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../screens/HomeScreen";
 
-type RootStackParamList = {
-    Home: undefined;
-    LogIn: undefined;
-    SignUp: undefined;
-    AfterLogin: undefined;
-    HomeScreen: undefined;
-  };
-  
-  type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
-  
-  type Props = {
-    navigation: HomeScreenNavigationProp;
-  };
-  
+type SignoutProps = {
+    onClick: () => void;
+}
 
 
-export default function SignOutButton({navigation}: Props) {
-
-    const handleSignOut = async () => {
-        const auth = getAuth();
-        try {
-            await auth
-             .signOut()
-             .then(() => navigation.navigate('HomeScreen'));
-
-        } catch (error) {
-            alert('Error signing out:');
-        }
-    };
-    
+export default function SignOutButton(props: SignoutProps) {
     return (
-        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <TouchableOpacity onPress={props.onClick} style={styles.button}>
             <Text style={styles.text}>Sign Out</Text>
         </TouchableOpacity>
     )
