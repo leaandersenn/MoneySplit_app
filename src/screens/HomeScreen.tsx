801a886f7483e6dae91ddca6f905b59513f63e5b
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-18 06:22:08 +0100

    Feat: Homescreen added createNewSplit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, Button, SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Button, SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { LargeText } from '../components/Text/LargeText';
 import { DocumentReference, DocumentSnapshot, collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
 import { SplitType, UserType } from '../utils/types';
@@ -7,7 +7,9 @@ import SplitCard from '../components/SplitCard';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { FIREBASE_AUTH, db } from '../../firebaseConfig';
 import SignOutButton from '../components/Buttons/SignOutButton';
-import { getAuth } from 'firebase/auth';
+import { User, getAuth } from 'firebase/auth';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Spacer from '../components/Spacer';
 
 
 export type RootStackParamList = {
@@ -15,8 +17,8 @@ export type RootStackParamList = {
     Split: {split: SplitType};
     LogIn: undefined;
     SignUp: undefined;
-    NewPayment: {split: SplitType, users: UserType[]};
-    CreateNewSplitScreen: {user: any};
+    NewPayment: {split: SplitType, users: UserType[], userId: string};
+    CreateNewSplitScreen: {user: UserType | undefined};
 }
 
 //type HomeScreenRouteProp = RouteProp<RootStackParamList, 'Home'>
@@ -29,67 +31,69 @@ type HomeScreenProps = {
 
 
 const HomeScreen = ({navigation}: HomeScreenProps) => {
-    const [user, setUser] = useState<UserType>()
-    const [data, setData] = useState<SplitType[]>([])
-    const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState<UserType>()
+  const [data, setData] = useState<SplitType[]>([])
+  const [loading, setLoading] = useState(true)
 
-    const currentUserEmail = FIREBASE_AUTH.currentUser?.email;
-    console.log('Current user: ', currentUserEmail);
-
-    useEffect(() => {
-      const fetchData = async () => {
-          if (!currentUserEmail) {
-              console.log('No current user email available');
-              return;
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const currentUserEmail = FIREBASE_AUTH.currentUser?.email;
+        if (currentUserEmail) {
+          const usersQuery = query(collection(db, 'Users'), where('email', '==', currentUserEmail));
+          const querySnapshot = await getDocs(usersQuery);
+          if (!querySnapshot.empty) {
+            const userData = querySnapshot.docs[0].data(); 
+            console.log(userData)
+            setUser(userData as UserType); 
+          } else {
+            console.log('Ingen dokumenter funnet med den e-postadressen');
           }
+        } else {
+          console.log('Ingen bruker er logget inn');
+        }
+      } catch (error) {
+        console.error('Error in fetchData:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
+
+  useEffect(() => {
+      const fetchData = async () => {
+        if (!user) {
+          console.log('User data is not available yet. No splits to render');
+          return;
+        }
           try {
-              const usersQuery = query(collection(db, 'Users'), where('email', '==', currentUserEmail));
-              console.log("heihei");
-              const querySnapshot = await getDocs(usersQuery);
-              if (!querySnapshot.empty) {
-                console.log("heiheihade");
-                  const userDoc = querySnapshot.docs[0]; 
-                  console.log('User document data:', userDoc.data());
-                  setUser(userDoc.data() as UserType); 
-                  console.log(userDoc.data() as UserType + "dette er faen brukeren")
-              } else {
-                  console.log('No user found with the given email');
+              const items = [];
+              for (const docRef of user.splits) {
+                  const docSnapshot = await getDoc(docRef)
+                  if (docSnapshot.exists()) {
+                      items.push({ ...docSnapshot.data(), id: docSnapshot.ref } as SplitType)
+                  }
               }
+              console.log(items)
+              setData(items)
+              console.log(data)
           } catch (error) {
-              console.error('Error fetching user document:', error);
+              console.error("Error fetching data: ", error)
+          }
+          finally{
+            setLoading(false)
           }
       };
+      
+      fetchData()
+  }, [user])
 
-      fetchData();
-  }, []);
-    
-    const handleCreateNewSplit = () => {
-      console.log('Add new split')
-      navigation.navigate('CreateNewSplitScreen', {user: user})
-    };
-
-    useEffect(() => {
-        const fetchData = async () => {
-          if (!user) {
-            console.log('User data is not available yet. No splits to render');
-            return;
-          }
-            try {
-                const items = [];
-                for (const docRef of user.splits) {
-                    const docSnapshot = await getDoc(docRef)
-                    if (docSnapshot.exists()) {
-                        items.push({ ...docSnapshot.data(), id: docSnapshot.ref } as SplitType)
-                    }
-                }
-                setData(items)
-            } catch (error) {
-                console.error("Error fetching data: ", error)
-            }
-        };
-        
-        fetchData()
-    }, [user])
+  const handleCreateNewSplit = () => {
+    console.log('Add new split')
+    navigation.navigate('CreateNewSplitScreen', {user: user})
+  };
 
     const handleSignOut = async () => {
       const auth = getAuth();
@@ -109,6 +113,12 @@ const HomeScreen = ({navigation}: HomeScreenProps) => {
     <View style={styles.container}>
       <View style={styles.title}>
         <LargeText>{`Splits`}</LargeText>
+        <TouchableOpacity
+          style={styles.greenButton}
+          onPress={handleCreateNewSplit}
+          >
+        <FontAwesome name="plus" color="white" size={20}/>  
+      </TouchableOpacity>
       </View>
       
       <>
@@ -125,6 +135,7 @@ const HomeScreen = ({navigation}: HomeScreenProps) => {
             })}
       </ScrollView>
       <SignOutButton onClick={handleSignOut}/>
+      <Spacer size={20} horizontal={false}/>
       </>
     </View>
   )
@@ -136,7 +147,7 @@ export default HomeScreen
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+     flex: 1, 
       backgroundColor: '#ffff'
     },
     cards: {
@@ -146,14 +157,22 @@ const styles = StyleSheet.create({
       marginTop: 120,
       alignItems: 'center'
     },
-    image: {
-      width: 100, 
-      height: 100, 
-    },
     title:{
       marginTop: 75, 
       marginLeft: 20,
       marginBottom: 10,
-      flexDirection: 'row'
+      marginRight: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+     
+    },
+    greenButton: {
+      backgroundColor: '#43B05C', 
+      width: 50,
+      height: 50,
+      borderRadius: 100,
+      justifyContent: 'center',
+      alignItems: 'center',
     }
   });
